a6e12b1fc5c3f875da884f28fe1f2c7b204f9c60
Gen'O <geno@GenOs-iMac.local>
2023-09-25 16:59:51 +0800

    test glsl

--- app/cnoise/mesh/cnoiseMesh.tsx
+++ app/cnoise/mesh/cnoiseMesh.tsx
@@ -1,42 +1,54 @@
-import { shaderMaterial } from "@react-three/drei";
-import { extend, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 import VertexShader from "../shaders/vertexShader";
 import FragmentShader from "../shaders/fragmentShader";
 import * as THREE from "three";
 
-const SphereMaterial = shaderMaterial(
-  {
-    uTime: 0,
-    randomFactors: [1, 1, 1],
-  },
-  VertexShader,
-  FragmentShader
-);
-
-extend({ SphereMaterial });
+const Uniforms = {
+  uTime: { value: 1 },
+  randomFactors: { value: [1, 1, 1] },
+};
 
 const CnoiseMesh = () => {
   const mesh = useRef<any>();
+  const shader = useRef<any>();
+  const geometryRef = useRef<any>();
 
   const ROTATION_SPEED = 0.02;
 
-  useFrame(({ clock }) => {
-    if (mesh.current) {
-      const elapsedTime = clock.getElapsedTime();
-
-      mesh.current.material.uniforms.uTime.value = elapsedTime;
-      mesh.current.rotation.y -= 0.002;
+  useEffect(() => {
+    if (geometryRef.current) {
+      const attributesCount = geometryRef.current.attributes.position.count;
+      const newRandomAttributes = new Float32Array(attributesCount);
+      for (let i = 0; i < attributesCount; i++)
+        newRandomAttributes[i] = Math.random();
+      geometryRef.current.setAttribute(
+        "aRandom",
+        new THREE.BufferAttribute(newRandomAttributes, 1)
+      );
     }
+  }, [geometryRef]);
+
+  useFrame((state, delta) => {
+    const { clock } = state;
+    const elapsedTime = clock.getElapsedTime();
+    shader.current.uniforms.uTime += elapsedTime;
+    mesh.current.rotation.y -= 0.002;
   });
 
   return (
-    <mesh position={[0, 0, 0]} ref={mesh} scale={0.5}>
-      <sphereGeometry args={[5, 128, 128]} />
-      {/* <icosahedronGeometry args={[5, 10]} /> */}
+    <mesh position={[0, 0, 0]} scale={0.5} ref={mesh}>
+      {/* <sphereGeometry ref={geometryRef} args={[10, 128, 128]} /> */}
+      <icosahedronGeometry ref={geometryRef} args={[10, 30]} />
       {/* <boxGeometry args={[5, 5, 5]} /> */}
-      {/* <torusKnotGeometry args={[10, 1, 100, 20, 9, 10]} /> */}
-      <sphereMaterial />
+      {/* <planeGeometry args={[10, 1, 100, 20, 9, 10]} /> */}
+      {/* <sphereMaterial /> */}
+      <shaderMaterial
+        vertexShader={VertexShader}
+        fragmentShader={FragmentShader}
+        ref={shader}
+        uniforms={Uniforms}
+      />
       {/* <meshStandardMaterial color={"red"} /> */}
     </mesh>
   );
--- app/sphereCanvas.tsx
+++ app/sphereCanvas.tsx
@@ -9,7 +9,7 @@ import CnoiseMesh from "./cnoise/mesh/cnoiseMesh";
 
 const SphereCanvas = () => {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 0, 10] }}>
       <ambientLight intensity={1} />
       <Suspense fallback={null}>
         {/* <TestSphere /> */}
